c8343ea1dea6de9f0149201aeb2329c9fad304e4
BerryHeyy <berkaykaplanbk@hotmail.com>
2022-12-09 13:44:53 +0100

    Updated resource handler

--- src/resource_handler.hpp
+++ src/resource_handler.hpp
@@ -5,5 +5,8 @@
 
 namespace hkm::resource_handler
 {
-    std::vector<char> read_file(const std::string& filename);
+    std::string get_working_directory();
+
+    std::vector<char> read_file_binary(const std::string& filename);
+    std::vector<char> read_shader_binary(const std::string& shader_name);
 }
